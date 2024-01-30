42b555a2f1fe08a3e4d015671bfcad9015b73c06
vahsimonian <simonianvah@gmail.com>
2024-01-30 13:00:41 +0700

    commit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,81 +1,80 @@
-import { useLogin } from '@refinedev/core';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
+import { useLogin } from "@refinedev/core";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-// import { ThemedTitleV2 } from '@refinedev/mui';
-// {/* <ThemedTitleV2
-//           collapsed={false}
-//           wrapperStyles={{
-//             fontSize: '22px',
-//             justifyContent: 'center',
-//           }}
-//         /> */}
+import { yariga } from "../assets";
 
-import { yariga } from 'assets';
-
-import { CredentialResponse } from '../interfaces/google';
-
-const GOOGLE_CLIENT_ID =
-  '1003117598679-gu7fvobjsefcfb0fovpambq51sjcq8k5.apps.googleusercontent.com';
+import { CredentialResponse } from "../interfaces/google";
 
 export const Login: React.FC = () => {
-  const { mutate: login } = useLogin<CredentialResponse>();
+    const { mutate: login } = useLogin<CredentialResponse>({
+        v3LegacyAuthProviderCompatible: true,
+    });
 
-  const GoogleButton = (): JSX.Element => {
-    const divRef = useRef<HTMLDivElement>(null);
+    const GoogleButton = (): JSX.Element => {
+        const divRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-      if (typeof window === 'undefined' || !window.google || !divRef.current) {
-        return;
-      }
-
-      try {
-        window.google.accounts.id.initialize({
-          ux_mode: 'popup',
-          client_id: GOOGLE_CLIENT_ID,
-          callback: async (res: CredentialResponse) => {
-            if (res.credential) {
-              login(res);
+        useEffect(() => {
+            if (
+                typeof window === "undefined" ||
+                !window.google ||
+                !divRef.current
+            ) {
+                return;
             }
-          },
-        });
-        window.google.accounts.id.renderButton(divRef.current, {
-          theme: 'filled_blue',
-          size: 'medium',
-          type: 'standard',
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }, []);
 
-    return <div ref={divRef} />;
-  };
+            try {
+                window.google.accounts.id.initialize({
+                    ux_mode: "popup",
+                    client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+                    callback: async (res: CredentialResponse) => {
+                        if (res.credential) {
+                            login(res);
+                        }
+                    },
+                });
+                window.google.accounts.id.renderButton(divRef.current, {
+                    theme: "filled_blue",
+                    size: "medium",
+                    type: "standard",
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        }, []); // you can also add your client id as dependency here
 
-  return (
-    <Container
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fcfcfc',
-      }}
-    >
-      <Box
-        display="flex"
-        gap="36px"
-        justifyContent="center"
-        flexDirection="column"
-      >
-        <Typography align="center" color={'text.secondary'} fontSize="12px">
-          <img alt="yariga" src={yariga} />
-        </Typography>
+        return <div ref={divRef} />;
+    };
 
-        <GoogleButton />
-      </Box>
-    </Container>
-  );
+    return (
+        <Box component="div" sx={{ backgroundColor: "#FCFCFC" }}>
+            <Container
+                component="main"
+                maxWidth="xs"
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    height: "100vh",
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <div>
+                        <img src={yariga} alt="Yariga Logo" />
+                    </div>
+                    <Box mt={4}>
+                        <GoogleButton />
+                    </Box>
+                </Box>
+            </Container>
+        </Box>
+    );
 };
